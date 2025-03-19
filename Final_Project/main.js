262f929799ed44f80e855f84ec6791c0521d9305
SergeyMaximov <mrknitter7@gmail.com>
2025-03-19 17:10:01 +0300

    Final project(no favourite list)

--- Final_Project/main.js
+++ Final_Project/main.js
@@ -1,23 +1,37 @@
+const colors = {
+    GREEN: '#C2F37D',
+    BLUE: '#7DE1F3',
+    RED: '#F37D7D',
+    YELLOW: '#F3DB7D',
+    PURPLE: '#E77DF3',
+};
+
 const model = {
     notes: [],
     
-    addNote(title, content) {
+    addNote(title, color, content) {
         const id = new Date().getTime();
         const isFavourite = false;
-        const newNote = {id, title, content, isFavourite};
+        
+        const newNote = {id, title, color, content, isFavourite};
+        
         
         this.notes.push(newNote);
-        view.renderNotes(this.notes);
+        view.renderNotes(color, id);
         view.renderNotesCount(this.notes);
-        view.showDone();
     },
     
-    showError() {
-        view.showError();
+    removeNote(noteId) {
+        this.notes = this.notes.filter(note => note.id !== noteId);
+        view.removeNoteById(noteId);
+        view.showRemoved();
+        view.renderNotesCount(this.notes);
     },
     
-    showNotFilled() {
-        view.showNotFilled();
+    toggleFavourite(noteId) {
+        const favouriteNote = this.notes.find(note => note.id === noteId);
+        favouriteNote.isFavourite = !favouriteNote.isFavourite;
+        view.toggleFavourite(noteId);
     },
     
     showNoNotes(notes) {
@@ -28,75 +42,64 @@ const model = {
             messageBox.style.color = 'darkred';
             messageBox.style.fontWeight = 500;
         }
-    }
-    
-    // favouriteNote(favouriteId) {
-    //     this.notes.forEach(note => {
-    //         if (note.id === favouriteId) {
-    //             const heartIcon = document.querySelector(`#${note.id} .heart`);
-    //             heartIcon.setAttribute('src', 'assets/heart_active.svg');
-    //         }
-    //
-    //     })
-    //     return this.notes;
-    // },
-    
-    // toggleFavourite(id) {
-    //     this.notes = this.notes.map(note => {
-    //         if (note.id === id) {
-    //             note.isFavourite = !note.isFavourite;
-    //         }
-    //         return note;
-    //     });
-    // },
+    },
 };
 
 const view = {
     init() {
-        // this.renderNotes(model.notes);
-
         const btn = document.querySelector('.add-note-btn');
         const noteTitle = document.getElementById('note-title');
         const noteContent = document.getElementById('note-content');
         const notesContainer = document.querySelector('.notesContainer');
-        
+        const colors = document.querySelector('.radio-list');
+        let noteColor = '#F3DB7D';
         //добавление заметки
         btn.addEventListener('click', event => {
             event.preventDefault();
             const title = noteTitle.value;
             const content = noteContent.value;
-
-            controller.addNote(title, content);
+    
+            controller.addNote(title, noteColor, content);
             noteTitle.value = '';
             noteContent.value = '';
-            controller.renderNotesCount(notes);
         });
-        //добавление заметки в избранное
-        notesContainer.addEventListener('click', event => {
-            if (event.target.classList.contains('heart')) {
-                const favouriteId = +event.target.closest('.new-note').id;
-                controller.favouriteNote(favouriteId);
+        
+        colors.addEventListener('click', event => {
+            if (event.target.classList.contains('radio')) {
+                noteColor = event.target.closest('.radio').value;
             }
-        })
+        });
+        
+        notesContainer.addEventListener('click', event => {
+            const noteId = +event.target.closest('.new-note').getAttribute('data-id');
+            if (event.target.classList.contains('trash')) {
+                controller.removeNote(noteId);
+            };
+            if (event.target.classList.contains('heart')) {
+                controller.toggleFavourite(noteId);
+            }
+        });
     },
     
     showNoNotes (notes) {
         controller.showNoNotes(notes);
-        // messageBox.textContent = `У вас нет еще ни одной заметки</br>
-        //                         Заполните поля выше и создайте свою первую заметку!`;
     },
 
-    renderNotes(notes) {
+    renderNotes(color, id) {
         const notesContainer = document.querySelector('.notesContainer');
+        
         const noteTitle = document.getElementById('note-title').value;
         const noteText = document.getElementById('note-content').value;
 
         const noteElement = document.createElement('div');
         noteElement.classList.add('new-note');
+        noteElement.setAttribute('data-id', id);
 
         const noteHeader = document.createElement('div');
         noteHeader.classList.add('new-note-header');
-        noteHeader.textContent = noteTitle;
+        
+        noteHeader.style.backgroundColor = color;
+        noteHeader.textContent = `${new Date().toLocaleDateString()} ${noteTitle}`;
 
         const noteTrash = document.createElement('span');
         noteTrash.classList.add('new-note-header-icons');
@@ -119,6 +122,24 @@ const view = {
         counter.textContent = notes.length;
     },
     
+    removeNoteById (id) {
+        const currentNote = document.querySelector(`[data-id="${id}"].new-note`);
+        if (currentNote){
+            currentNote.remove();
+        };
+    },
+    
+    toggleFavourite(noteId) {
+        const noteElement = document.querySelector(`.new-note[data-id="${noteId}"]`);
+        const heartIcon = noteElement.querySelector('.heart');
+        
+        if (heartIcon.src.includes('heart_inactive.svg')) {
+            heartIcon.src = 'assets/heart_active.svg';
+        } else {
+            heartIcon.src = 'assets/heart_inactive.svg';
+        }
+    },
+    
     showDone() {
         const messageBox = document.querySelector('.message-box');
         
@@ -138,30 +159,37 @@ const view = {
     
         messageBox.innerHTML = `<img src="assets/message_Not_filled.svg" alt=""/>`;
         setTimeout(() => messageBox.innerHTML = '', 3000);
+    },
+    
+    showRemoved () {
+        const messageBox = document.querySelector('.message-box');
+    
+        messageBox.innerHTML = `<img src="assets/message_Removed.svg" alt=""/>`;
+        setTimeout(() => messageBox.innerHTML = '', 3000);
     }
-    
-    
 };
 
 const controller = {
-    addNote(title, content) {
+    addNote(title, color, content) {
         const savedTitle = title;
         const savedContent = content;
         
         if (title.trim().length > 15) {
-            model.showError();
+            view.showError();
             setTimeout(() => {
                 document.getElementById('note-title').value = savedTitle;
                 document.getElementById('note-content').value = savedContent;
             }, 0);
         } else if (!title || title.trim() === '' || !content || content.trim() === '') {
-            model.showNotFilled();
+            view.showNotFilled();
             setTimeout(() => {
                 document.getElementById('note-title').value = savedTitle;
                 document.getElementById('note-content').value = savedContent;
             }, 0);
         } else {
-            model.addNote(title, content);
+            model.addNote(title, color,content);
+            view.showDone();
+    
             document.getElementById('note-title').value = '';
             document.getElementById('note-content').value = '';
         }
@@ -173,6 +201,14 @@ const controller = {
     
     showNoNotes(notes) {
         model.showNoNotes(notes);
+    },
+    
+    removeNote(noteId) {
+        model.removeNote(noteId);
+    },
+    
+    toggleFavourite(noteId) {
+        model.toggleFavourite(noteId);
     }
     
 };
